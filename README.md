d023aa3e885b8c1204ee055d3eada9098cd0b4ee
HA7ILM <retzlerandras@gmail.com>
2014-03-26 00:36:13 +0100

    Upload project files

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 minidemod
 =========
 
-A really-really simple demodulator intended to teach Software Defined Radio to newcomers.
+The purpose of <tt>minidemod-wfm</tt> is to have a C program with a few lines that can actually demodulate WFM from an I/Q input. 
+
+The whole DSP chain is built by OS pipes, just like this:
+
+<tt>rtl_sdr -s 250000 -f 89500000 -g 20 - | tcc -lm -run minidemod-wfm.c | sox -t raw -r 250000 -e unsigned -b 8 -c 1 - -t raw - rate 48000 | mplayer -quiet -rawaudio samplesize=1:channels=1:rate=48000 -demuxer rawaudio -</tt>
+
+Read like this:
+
+<tt>rtl_sdr (acquires samples) | minidemod-wfm (demodulates) | sox (low pass filter + decimation) | mplayer (audio output) </tt>
+
+##Authors
+
+András Retzler <ha7ilm@sdr.hu>
--- minidemod-wfm.c
+++ minidemod-wfm.c
@@ -1,4 +1,4 @@
-The MIT License (MIT)
+/*
 
 Copyright (c) 2014 Andras Retzler <ha7ilm@sdr.hu>
 
@@ -19,3 +19,37 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+-----------------------------------------------------------------------------
+
+To play with mplayer (recommended):
+
+rtl_sdr -s 250000 -f 89500000 -g 20 - | tcc -lm -run minidemod-wfm.c | sox -t raw -r 250000 -e unsigned -b 8 -c 1 - -t raw - rate 48000 | mplayer -quiet -rawaudio samplesize=1:channels=1:rate=48000 -demuxer rawaudio -
+
+To play with ALSA:
+
+rtl_sdr -s 240000 -f 89500000 -g 20 - | tcc -lm -run minidemod-wfm.c | sox -t raw -r 250000 -e unsigned -b 8 -c 1 - -t raw - rate 48000 | aplay -f U8 -c1 -r 48000 --buffer-size=200000
+
+Some parameters of rtl_sdr - e.g. frequency and gain - should be acquired by experimentation with a correct SDR software (gqrx or SDR#).
+
+*/
+
+#include <math.h>
+
+int main()
+{
+	double i1=0, i2, q1=0, q2, s;
+	for(;;)
+	{
+		i2=((unsigned char)getchar()-127); q2=((unsigned char)getchar()-127);
+		
+		//Formula: http://www.dsprelated.com/showmessage/142012/1.php
+		s=100*((i2*(q2-q1)) - (q2*(i2-i1)))/(i2*i2+q2*q2);
+
+		//Formula: phase change	
+		//s=100*(atan(i2/q2)-atan(i1/q1));
+
+		putchar((unsigned char)(s+127));
+		i1=i2; q1=q2;
+	}
+}
